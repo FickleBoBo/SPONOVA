8537ec0b25d0324e58f9667ab5644998dfd3d6d5
ghlim00 <p1ayer.one.potter@gmail.com>
2024-05-14 14:51:58 +0900

    [gh] community stores, views 생성

---
+++ SSAFY/Front-end/vue-project/src/stores/community.js
@@ -0,0 +1,53 @@
+import router from '@/router'
+import axios from 'axios'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+
+const GENS_API=`http://localhost:8080/newsports/community/posts`
+
+export const useCommunityStore = defineStore('post', () => {
+
+  const createPost = function(post){
+    axios({
+      url: GENS_API,
+      method: 'POST',
+      data: post
+    })
+    .then(()=> {
+      router.push({name: 'postList'})
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  const postList = ref([])
+  const getPostList = function(){
+    axios.get(GENS_API)
+    .then((response) =>{
+      postList.value = response.data
+    })
+  }
+
+  const post = ref({})
+  const getPost = function(postID){
+    axios.get(`${GENS_API}/${postID}`)
+    .then((response) => {
+      post.value = response.data
+    })
+  }
+
+  const updatePost = function(searchCondition){
+    axios.put(GENS_API, post.value)
+    .then(() => {
+      router.push({name: 'postList'})
+    })
+  }
+
+
+  return { createPost, postList, getPostList, post, getPost, updatePost}
+
+
+
+})
--- SSAFY/Front-end/vue-project/src/views/CommunityView.vue
+++ SSAFY/Front-end/vue-project/src/views/CommunityView.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
         <h1>CommunityView</h1>
+        <PostList/>
     </div>
 </template>
 
 
 
 <script setup>
+import PostList from '@/components/community/PostList.vue';
+
 
 </script>
 
