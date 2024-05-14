c9d0a070d05ed681dd7459539d8fe4ce44bcef83
ghlim00 <p1ayer.one.potter@gmail.com>
2024-05-14 14:50:25 +0900

    [gh] components/community/ 추가

---
+++ SSAFY/Front-end/vue-project/src/components/community/PostCreate.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <fieldset>
+          <legend>게시글 등록</legend>
+          <div>
+              <label for="title">제목 : </label>
+              <input type="text" id="title" v-model="post.title">
+          </div>
+          <div>
+              <label for="writer">쓰니 : </label>
+              <input type="text" id="writer" v-model="post.writer">
+          </div>
+          <div>
+              <label for="content">내용 : </label>
+              <textarea id="content" cols="30" rows="10" v-model="post.content"></textarea>
+          </div>
+          <div>
+              <!--createBoard는 메서드 명-->
+              <button @click="createPost">등록</button>
+          </div>
+      </fieldset>
+    </div>
+</template>
+
+
+
+
+<script setup>
+import { useCommunityStore } from '@/stores/community'
+import {ref} from 'vue'
+
+const store = useCommunityStore()
+
+const post = ref({
+  title: '',
+  content: ''
+})
+
+
+const createPost = function () {
+  store.createPost(post.value)
+}
+
+</script>
---
+++ SSAFY/Front-end/vue-project/src/components/community/PostList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <h4>커뮤니티 메인 페이지</h4>
+      <hr>
+      <table>
+          <tr>
+              <th>번호</th>
+              <th>제목</th>
+              <th>user</th>
+              <th>조회수</th>
+              <th>등록일</th>
+          </tr>
+          <tr v-for="post in store.postList" :key="post.postID">
+              <td>{{ post.postID }}</td>
+              <td>{{ post.postTitle }}</td>
+              <td>{{ post.userID }}</td>
+              <td>{{ post.postViewCnt }}</td>
+              <td>{{ post.postRegDate }}</td>
+          </tr>
+      </table>
+
+      <BoardSearchInput/>
+  </div>
+</template>
+
+
+
+
+
+<script setup>
+
+import { useCommunityStore } from '@/stores/community'
+import{onMounted} from 'vue'
+
+const store = useCommunityStore()
+
+onMounted(() => {
+  store.getPostList()
+})
+
+</script>
+
+
+
+
+
+<style scoped>
+</style>
