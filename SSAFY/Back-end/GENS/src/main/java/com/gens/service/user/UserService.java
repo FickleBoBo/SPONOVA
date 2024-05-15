18c3a9379e3545e3c8d983f806bf78e703b1244a
ghlim00 <p1ayer.one.potter@gmail.com>
2024-05-15 14:05:38 +0900

    [gh] 댓글에 대한 기본 CRUD 기능

--- SSAFY/Back-end/GENS/src/main/java/com/gens/service/user/UserService.java
+++ SSAFY/Back-end/GENS/src/main/java/com/gens/service/user/UserService.java
@@ -1,5 +1,15 @@
 package com.gens.service.user;
 
+import java.util.List;
+
+import com.gens.dto.user.User;
+
 public interface UserService {
+	
+	// 전체
+	List<User> getUserList();
+	
+	// 로그인
+	User login(String userID, String userPW);
 
 }
--- SSAFY/Back-end/GENS/src/main/java/com/gens/service/user/UserServiceImpl.java
+++ SSAFY/Back-end/GENS/src/main/java/com/gens/service/user/UserServiceImpl.java
@@ -1,5 +1,38 @@
 package com.gens.service.user;
 
-public class UserServiceImpl {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gens.dao.user.UserDao;
+import com.gens.dto.user.User;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	private final UserDao userDao;
+	
+	@Autowired
+	public UserServiceImpl(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Override
+	public List<User> getUserList() {
+		return userDao.selectAll();
+	}
+
+	@Override
+	public User login(String userID, String userPW) {
+		Map<String, String> info = new HashMap<>();
+		info.put("userID", userID);
+		info.put("userPW", userPW);
+		return userDao.selectOne(info);
+	}
+	
+	
 
 }
