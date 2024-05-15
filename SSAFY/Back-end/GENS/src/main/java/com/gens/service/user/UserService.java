0eed90973bd86d0ae8022c4ee92ffdc5097213ee
ghlim00 <p1ayer.one.potter@gmail.com>
2024-05-15 20:15:12 +0900

    [GH] 백엔드 User 관련 모든 파일: 로그인/로그아웃/회원가입 수정

--- SSAFY/Back-end/GENS/src/main/java/com/gens/service/user/UserService.java
+++ SSAFY/Back-end/GENS/src/main/java/com/gens/service/user/UserService.java
@@ -6,10 +6,13 @@ import com.gens.dto.user.User;
 
 public interface UserService {
 	
-	// 전체
+	// 전체 회원 조회
 	List<User> getUserList();
 	
-	// 로그인
+	// 회원 가입 하기
+	void signup(User user);
+	
+	// 로그인 하기
 	User login(String userID, String userPW);
 
 }
