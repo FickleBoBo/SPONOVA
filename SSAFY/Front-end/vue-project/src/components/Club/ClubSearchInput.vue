add5eab19be42dd0c1887ca19bba46dfd5579b74
ghlim00 <p1ayer.one.potter@gmail.com>
2024-05-16 15:51:18 +0900

    [GH] 클럽 종목별 조회 수정

---
+++ SSAFY/Front-end/vue-project/src/components/Club/ClubSearchInput.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="search">
+      <div>
+          <label>종목을 검색하세요 : </label>
+          <input type="text" v-model="searchInfo.word" />
+      </div>
+      <!-- <div>
+          <label>정렬 기준 :</label>
+          <select v-model="searchInfo.orderBy">
+              <option value='none'>없음</option>
+              <option value="title">제목</option>
+          </select>
+      </div> -->
+      <!-- <div>
+          <label>정렬 방향 :</label>
+          <select v-model="searchInfo.orderByDir">
+              <option value="asc">오름차순</option>
+              <option value="desc">내림차순</option>
+          </select>
+      </div> -->
+      <div>
+          <button @click="searchClubList"> 검색 </button>
+      </div>
+  </div>
+</template>
+
+<script setup>
+
+import {useClubStore} from '@/stores/club'
+import {ref} from 'vue'
+
+const searchInfo = ref({
+  key: 'gameID',
+  word: '', 
+  // orderBy: 'none',
+  // orderByDir: 'asc'
+})
+
+const clubStore = useClubStore()
+const searchClubList = function(){
+  clubStore.searchClubList(searchInfo.value)
+}
+
+</script>
+
+<style scoped>
+.search {
+  display: flex;
+}
+</style>
