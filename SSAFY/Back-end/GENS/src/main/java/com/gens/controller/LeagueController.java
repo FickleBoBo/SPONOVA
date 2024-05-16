5328db1ea68278f06afe7ec1ca31a0a665f985f8
ghlim00 <p1ayer.one.potter@gmail.com>
2024-05-16 21:56:07 +0900

    [MW] league 관련 전제 조회 백엔드 기능

--- SSAFY/Back-end/GENS/src/main/java/com/gens/controller/LeagueController.java
+++ SSAFY/Back-end/GENS/src/main/java/com/gens/controller/LeagueController.java
@@ -1,5 +1,35 @@
 package com.gens.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gens.dto.league.League;
+import com.gens.service.league.LeagueService;
+
+@RestController
+@RequestMapping("/newsports/league")
+@CrossOrigin("*")
 public class LeagueController {
+	
+	private LeagueService leagueService;
+	
+	@Autowired
+	public LeagueController(LeagueService leagueService) {
+		this.leagueService = leagueService;
+	}
+	
+	// 전체 조회
+	@GetMapping
+	public ResponseEntity<?> list(){
+		List<League> list = leagueService.getLeagueList();
+		return new ResponseEntity<List<League>>(list, HttpStatus.OK);
+	}
 
 }
--- SSAFY/Back-end/GENS/src/main/java/com/gens/service/league/LeagueServiceImpl.java
+++ SSAFY/Back-end/GENS/src/main/java/com/gens/service/league/LeagueServiceImpl.java
@@ -1,5 +1,27 @@
 package com.gens.service.league;
 
-public class LeagueServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gens.dao.league.LeagueDao;
+import com.gens.dto.league.League;
+
+@Service
+public class LeagueServiceImpl implements LeagueService{
+
+	private final LeagueDao leagueDao;
+	
+	@Autowired
+	public LeagueServiceImpl(LeagueDao leagueDao) {
+		this.leagueDao = leagueDao;
+	}
+	
+	
+	@Override
+	public List<League> getLeagueList() {
+		return leagueDao.selectAll();
+	}
 
 }
