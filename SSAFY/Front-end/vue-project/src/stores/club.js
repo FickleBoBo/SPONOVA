eb7bb2f3f7fb9369f44dde9e44a0b98ea10fad62
ghlim00 <p1ayer.one.potter@gmail.com>
2024-05-16 15:46:59 +0900

    [GH] club 정보 카드 형식으로 출력

--- SSAFY/Front-end/vue-project/src/stores/club.js
+++ SSAFY/Front-end/vue-project/src/stores/club.js
@@ -9,6 +9,7 @@ export const useClubStore = defineStore('club', () => {
 
 
   const clubList = ref([])
+
   const getClubList = function(){
     axios.get(GENS_API)
     .then((response) =>{
@@ -29,11 +30,33 @@ export const useClubStore = defineStore('club', () => {
     })
   }
 
+  const searchClubList = function(searchCondition) {
+
+    let url=GENS_API
+    // 검색어가 있을 때만 검색 기능을 작동시킬 수 있다
+    if (searchCondition.key !== 'none' && searchCondition.word) {
+      url += `/search`; 
+    }
+
+    axios.get(url, {
+      params: searchCondition
+    })
+    .then((response) => {
+      clubList.value = response.data
+      console.log(clubList.value)
+    })
+    .catch((error) => {
+      console.error('Error searching posts:', error);
+    });
+  }
+
 
   return { 
     clubList, 
     getClubList, 
-    club 
+    club,
+    getClub,
+    searchClubList
   }
 
 
